dd27599b1f24877ed311cae851d46eb8c08c40d6
gwilymr <31447106+gwilymr@users.noreply.github.com>
2018-04-02 23:09:40 +0100

    Reset to version adding full stop

--- run.sh
+++ run.sh
@@ -6,22 +6,12 @@ set -x
 java -cp ${H2_HOME}/h2/bin/h2*.jar org.h2.tools.Server \
     -web -webAllowOthers -webPort 8080 \
     -tcp -tcpAllowOthers -tcpPort 1521 \
-    -baseDir ${H2_HOME}/h2-data &
-
-sleep 10 
+    -baseDir ${H2_HOME}/h2-data | \
 
 java -cp ${H2_HOME}/h2/bin/h2*.jar org.h2.tools.RunScript \
     -url jdbc:h2:tcp://localhost:1521/~/${DATABASE_NAME} \
     -script ${H2_HOME}/init/init.sql \
     -showResults \
     -user sa
-    
-java -cp ${H2_HOME}/h2/bin/h2*.jar org.h2.tools.Console \
-    -web -webAllowOthers -webPort 8082 
-
-
-
-echo "$@"
-echo ======
 
 exec "$@";
